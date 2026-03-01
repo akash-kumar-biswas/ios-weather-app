2c97231e79ce062b53e350342cef6987250e1b0b
macos <macos@macoss-Mac.local>
2026-02-28 23:50:04 -0800

    chore: initial SwiftUI project setup

---
+++ Weather-App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather-App
+//
+//  Created by macos on 28/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
